1a8c90f3e184195814e85cb23ac0d1963941e69a
aiWen_Company <yuanyuan_pyy@163.com>
2017-03-20 10:03:48 +0800

    创建项目
    Signed-off-by: aiWen_Company <yuanyuan_pyy@163.com>

---
+++ NotificationInApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NotificationInApp
+//
+//  Created by Snake on 17/3/20.
+//  Copyright © 2017年 IAsk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NotificationInApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NotificationInApp
+//
+//  Created by Snake on 17/3/20.
+//  Copyright © 2017年 IAsk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
